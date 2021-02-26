c3d27cb8df2571c8b6a8f0d6016b661e030c1464
Nitish Sachar <nitish.sachar@protonmail.com>
2021-02-25 22:00:04 -0800

    20

---
+++ python3/easy/20-valid-parentheses.py
@@ -0,0 +1,97 @@
+#
+# @lc app=leetcode id=20 lang=python3
+#
+# [20] Valid Parentheses
+#
+# https://leetcode.com/problems/valid-parentheses/description/
+#
+# algorithms
+# Easy (39.86%)
+# Likes:    6874
+# Dislikes: 283
+# Total Accepted:    1.3M
+# Total Submissions: 3.3M
+# Testcase Example:  '"()"'
+#
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and
+# ']', determine if the input string is valid.
+#
+# An input string is valid if:
+#
+#
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+#
+#
+#
+# Example 1:
+#
+#
+# Input: s = "()"
+# Output: true
+#
+#
+# Example 2:
+#
+#
+# Input: s = "()[]{}"
+# Output: true
+#
+#
+# Example 3:
+#
+#
+# Input: s = "(]"
+# Output: false
+#
+#
+# Example 4:
+#
+#
+# Input: s = "([)]"
+# Output: false
+#
+#
+# Example 5:
+#
+#
+# Input: s = "{[]}"
+# Output: true
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= s.length <= 10^4
+# s consists of parentheses only '()[]{}'.
+#
+#
+#
+
+# @lc code=start
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        for i in range(len(s)):
+            if s[i] == "(" or s[i] == "[" or s[i] == "{":
+                stack.append(s[i])
+            else:
+                if not stack:
+                    return False
+                entry = stack.pop()
+                if (
+                    entry == "["
+                    and s[i] != "]"
+                    or entry == "{"
+                    and s[i] != "}"
+                    or entry == "("
+                    and s[i] != ")"
+                ):
+                    return False
+
+        return not stack
+
+
+# @lc code=end
