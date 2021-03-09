8cac36581a145e89bb1d46270cba1f7e3cbc0eb7
Nitish Sachar <nitish.sachar@protonmail.com>
2021-03-08 20:57:00 -0800

    215

---
+++ python3/medium/215-kth-largest-element-in-an-array.py
@@ -0,0 +1,56 @@
+#
+# @lc app=leetcode id=215 lang=python3
+#
+# [215] Kth Largest Element in an Array
+#
+# https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+#
+# algorithms
+# Medium (58.35%)
+# Likes:    5249
+# Dislikes: 337
+# Total Accepted:    845K
+# Total Submissions: 1.4M
+# Testcase Example:  '[3,2,1,5,6,4]\n2'
+#
+# Given an integer array nums and an integer k, return the k^th largest element
+# in the array.
+#
+# Note that it is the k^th largest element in the sorted order, not the k^th
+# distinct element.
+#
+#
+# Example 1:
+# Input: nums = [3,2,1,5,6,4], k = 2
+# Output: 5
+# Example 2:
+# Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+# Output: 4
+#
+#
+# Constraints:
+#
+#
+# 1 <= k <= nums.length <= 10^4
+# -10^4 <= nums[i] <= 10^4
+#
+#
+#
+
+# @lc code=start
+from heapq import *
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = []
+        for num in nums:
+            heappush(heap, -num)
+
+        for i in range(k - 1):
+            heappop(heap)
+
+        return -heap[0]
+
+
+# @lc code=end
