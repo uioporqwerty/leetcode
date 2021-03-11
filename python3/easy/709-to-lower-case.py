613dd4a8d85ae990b135a2211f892c5eb398a3ba
Nitish Sachar <nitish.sachar@protonmail.com>
2021-03-10 21:12:22 -0800

    709

---
+++ python3/easy/709-to-lower-case.py
@@ -0,0 +1,55 @@
+#
+# @lc app=leetcode id=709 lang=python3
+#
+# [709] To Lower Case
+#
+# https://leetcode.com/problems/to-lower-case/description/
+#
+# algorithms
+# Easy (80.06%)
+# Likes:    643
+# Dislikes: 1817
+# Total Accepted:    256K
+# Total Submissions: 319.3K
+# Testcase Example:  '"Hello"'
+#
+# Implement function ToLowerCase() that has a string parameter str, and returns
+# the same string in lowercase.
+#
+#
+#
+#
+# Example 1:
+#
+#
+# Input: "Hello"
+# Output: "hello"
+#
+#
+#
+# Example 2:
+#
+#
+# Input: "here"
+# Output: "here"
+#
+#
+#
+# Example 3:
+#
+#
+# Input: "LOVELY"
+# Output: "lovely"
+#
+#
+#
+#
+#
+
+# @lc code=start
+class Solution:
+    def toLowerCase(self, str: str) -> str:
+        return str.lower()
+
+
+# @lc code=end
