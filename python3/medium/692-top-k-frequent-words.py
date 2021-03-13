5ad0bac1105a9cecd3d219d5bb9f3bcff0b780d5
Nitish Sachar <nitish.sachar@protonmail.com>
2021-03-13 01:16:45 -0800

    692

---
+++ python3/medium/692-top-k-frequent-words.py
@@ -0,0 +1,74 @@
+#
+# @lc app=leetcode id=692 lang=python3
+#
+# [692] Top K Frequent Words
+#
+# https://leetcode.com/problems/top-k-frequent-words/description/
+#
+# algorithms
+# Medium (53.06%)
+# Likes:    2790
+# Dislikes: 186
+# Total Accepted:    261.3K
+# Total Submissions: 491.7K
+# Testcase Example:  '["i", "love", "leetcode", "i", "love", "coding"]\n2'
+#
+# Given a non-empty list of words, return the k most frequent elements.
+# Your answer should be sorted by frequency from highest to lowest. If two
+# words have the same frequency, then the word with the lower alphabetical
+# order comes first.
+#
+# Example 1:
+#
+# Input: ["i", "love", "leetcode", "i", "love", "coding"], k = 2
+# Output: ["i", "love"]
+# Explanation: "i" and "love" are the two most frequent words.
+# ⁠   Note that "i" comes before "love" due to a lower alphabetical order.
+#
+#
+#
+# Example 2:
+#
+# Input: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is",
+# "is"], k = 4
+# Output: ["the", "is", "sunny", "day"]
+# Explanation: "the", "is", "sunny" and "day" are the four most frequent words,
+# ⁠   with the number of occurrence being 4, 3, 2 and 1 respectively.
+#
+#
+#
+# Note:
+#
+# You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+# Input words contain only lowercase letters.
+#
+#
+#
+# Follow up:
+#
+# Try to solve it in O(n log k) time and O(n) extra space.
+#
+#
+#
+
+# @lc code=start
+from collections import Counter
+from heapq import *
+
+
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        frequencies = Counter(words)
+        heap = []
+        for frequency in frequencies:
+            heappush(heap, (-frequencies[frequency], frequency))
+
+        result = []
+        for i in range(k):
+            if heap:
+                result.append(heappop(heap)[1])
+
+        return result
+
+
+# @lc code=end
