8948be85434acf9d863aab2f11e8ed90f706f820
Nitish Sachar <nitish.sachar@protonmail.com>
2021-03-20 18:15:50 -0700

    1527

---
+++ mysql/easy/1527-patients-with-a-condition.sql
@@ -0,0 +1,73 @@
+--
+-- @lc app=leetcode id=1527 lang=mysql
+--
+-- [1527] Patients With a Condition
+--
+-- https://leetcode.com/problems/patients-with-a-condition/description/
+--
+-- database
+-- Easy (68.09%)
+-- Likes:    22
+-- Dislikes: 53
+-- Total Accepted:    8.9K
+-- Total Submissions: 13.7K
+-- Testcase Example:  '{"headers": {"Patients": ["patient_id", "patient_name", "conditions"]}, "rows": {"Patients": [[1, "Daniel", "YFEV COUGH"], [2, "Alice", ""], [3, "Bob", "DIAB100 MYOP"], [4, "George", "ACNE DIAB100"], [5, "Alain", "DIAB201"]]}}'
+--
+-- Table: Patients
+--
+--
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | patient_id   | int     |
+-- | patient_name | varchar |
+-- | conditions   | varchar |
+-- +--------------+---------+
+-- patient_id is the primary key for this table.
+-- 'conditions' contains 0 or more code separated by spaces.
+-- This table contains information of the patients in the hospital.
+--
+--
+--
+--
+-- Write an SQL query to report the patient_id, patient_name all conditions of
+-- patients who have Type I Diabetes. Type I Diabetes always starts with DIAB1
+-- prefix
+--
+-- Return the result table in any order.
+--
+-- The query result format is in the following example.
+--
+--
+--
+--
+-- Patients
+-- +------------+--------------+--------------+
+-- | patient_id | patient_name | conditions   |
+-- +------------+--------------+--------------+
+-- | 1          | Daniel       | YFEV COUGH   |
+-- | 2          | Alice        |              |
+-- | 3          | Bob          | DIAB100 MYOP |
+-- | 4          | George       | ACNE DIAB100 |
+-- | 5          | Alain        | DIAB201      |
+-- +------------+--------------+--------------+
+--
+-- Result table:
+-- +------------+--------------+--------------+
+-- | patient_id | patient_name | conditions   |
+-- +------------+--------------+--------------+
+-- | 3          | Bob          | DIAB100 MYOP |
+-- | 4          | George       | ACNE DIAB100 |
+-- +------------+--------------+--------------+
+-- Bob and George both have a condition that starts with DIAB1.
+--
+--
+--
+
+-- @lc code=start
+select patient_id, patient_name, conditions
+from patients
+where conditions like 'DIAB1%' or conditions like '% DIAB1%'
+
+-- @lc code=end
+
