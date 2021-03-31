24c1e70222c3328d3888a3b8e64f8bbcd86f9655
Nitish Sachar <nitish.sachar@protonmail.com>
2021-03-31 09:30:30 -0700

    1243

---
+++ python3/easy/1243-array-transformation.py
@@ -0,0 +1,91 @@
+#
+# @lc app=leetcode id=1243 lang=python3
+#
+# [1243] Array Transformation
+#
+# https://leetcode.com/problems/array-transformation/description/
+#
+# algorithms
+# Easy (50.15%)
+# Likes:    79
+# Dislikes: 40
+# Total Accepted:    8.1K
+# Total Submissions: 16.2K
+# Testcase Example:  '[6,2,3,4]\r'
+#
+# Given an initial array arr, every day you produce a new array using the array
+# of the previous day.
+#
+# On the i-th day, you do the following operations on the array of day i-1 to
+# produce the array of day i:
+#
+#
+# If an element is smaller than both its left neighbor and its right neighbor,
+# then this element is incremented.
+# If an element is bigger than both its left neighbor and its right neighbor,
+# then this element is decremented.
+# The first and last elements never change.
+#
+#
+# After some days, the array does not change. Return that final array.
+#
+#
+# Example 1:
+#
+#
+# Input: arr = [6,2,3,4]
+# Output: [6,3,3,4]
+# Explanation:
+# On the first day, the array is changed from [6,2,3,4] to [6,3,3,4].
+# No more operations can be done to this array.
+#
+#
+# Example 2:
+#
+#
+# Input: arr = [1,6,3,4,3,5]
+# Output: [1,4,4,4,4,5]
+# Explanation:
+# On the first day, the array is changed from [1,6,3,4,3,5] to [1,5,4,3,4,5].
+# On the second day, the array is changed from [1,5,4,3,4,5] to [1,4,4,4,4,5].
+# No more operations can be done to this array.
+#
+#
+#
+# Constraints:
+#
+#
+# 3 <= arr.length <= 100
+# 1 <= arr[i] <= 100
+#
+#
+#
+
+# @lc code=start
+class Solution:
+    def transformArray(self, arr: List[int]) -> List[int]:
+        previous_day_arr = list(arr)
+        while True:
+            operations_performed = False
+            current_day_arr = list(previous_day_arr)
+            for i in range(1, len(previous_day_arr) - 1):
+                if (
+                    previous_day_arr[i] < previous_day_arr[i - 1]
+                    and previous_day_arr[i] < previous_day_arr[i + 1]
+                ):
+                    current_day_arr[i] += 1
+                    operations_performed = True
+                elif (
+                    previous_day_arr[i] > previous_day_arr[i - 1]
+                    and previous_day_arr[i] > previous_day_arr[i + 1]
+                ):
+                    current_day_arr[i] -= 1
+                    operations_performed = True
+            previous_day_arr = current_day_arr
+            if not operations_performed:
+                break
+
+        return previous_day_arr
+
+
+# @lc code=end
