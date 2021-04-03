832b6d78fdab175da578a25c5079f2e409e8824f
Nitish Sachar <nitish.sachar@protonmail.com>
2021-04-03 01:00:14 -0700

    1539

---
+++ python3/easy/1539-kth-missing-positive-number.py
@@ -0,0 +1,70 @@
+#
+# @lc app=leetcode id=1539 lang=python3
+#
+# [1539] Kth Missing Positive Number
+#
+# https://leetcode.com/problems/kth-missing-positive-number/description/
+#
+# algorithms
+# Easy (55.07%)
+# Likes:    846
+# Dislikes: 44
+# Total Accepted:    69.1K
+# Total Submissions: 125.8K
+# Testcase Example:  '[2,3,4,7,11]\n5'
+#
+# Given an array arr of positive integers sorted in a strictly increasing
+# order, and an integer k.
+#
+# Find the k^th positive integer that is missing from this array.
+#
+#
+# Example 1:
+#
+#
+# Input: arr = [2,3,4,7,11], k = 5
+# Output: 9
+# Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The
+# 5^th missing positive integer is 9.
+#
+#
+# Example 2:
+#
+#
+# Input: arr = [1,2,3,4], k = 2
+# Output: 6
+# Explanation: The missing positive integers are [5,6,7,...]. The 2^nd missing
+# positive integer is 6.
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= arr.length <= 1000
+# 1 <= arr[i] <= 1000
+# 1 <= k <= 1000
+# arr[i] < arr[j] for 1 <= i < j <= arr.length
+#
+#
+#
+
+# @lc code=start
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        # Time: O(n)
+        # Space: O(n)
+        numbers = set(arr)
+        i = 1
+        missing_number_count = 1
+        while True:
+            if i not in numbers:
+                if missing_number_count == k:
+                    return i
+                missing_number_count += 1
+            i += 1
+
+        return -1
+
+
+# @lc code=end
