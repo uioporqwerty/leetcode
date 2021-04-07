36b67ecfffbbe02bfd53f8b3831c07c4d0680e0e
Nitish Sachar <nitish.sachar@protonmail.com>
2021-04-06 21:16:27 -0700

    1365

---
+++ python3/easy/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -0,0 +1,90 @@
+#
+# @lc app=leetcode id=1365 lang=python3
+#
+# [1365] How Many Numbers Are Smaller Than the Current Number
+#
+# https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/description/
+#
+# algorithms
+# Easy (85.99%)
+# Likes:    1430
+# Dislikes: 36
+# Total Accepted:    172.4K
+# Total Submissions: 200.5K
+# Testcase Example:  '[8,1,2,2,3]'
+#
+# Given the array nums, for each nums[i] find out how many numbers in the array
+# are smaller than it. That is, for each nums[i] you have to count the number
+# of valid j's such that j != i and nums[j] < nums[i].
+#
+# Return the answer in an array.
+#
+#
+# Example 1:
+#
+#
+# Input: nums = [8,1,2,2,3]
+# Output: [4,0,1,1,3]
+# Explanation:
+# For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3).
+# For nums[1]=1 does not exist any smaller number than it.
+# For nums[2]=2 there exist one smaller number than it (1).
+# For nums[3]=2 there exist one smaller number than it (1).
+# For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+#
+#
+# Example 2:
+#
+#
+# Input: nums = [6,5,4,8]
+# Output: [2,1,0,3]
+#
+#
+# Example 3:
+#
+#
+# Input: nums = [7,7,7,7]
+# Output: [0,0,0,0]
+#
+#
+#
+# Constraints:
+#
+#
+# 2 <= nums.length <= 500
+# 0 <= nums[i] <= 100
+#
+#
+#
+
+# @lc code=start
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        # Time: O(n^2)
+        # Space: O(1)
+        # result = [0] * len(nums)
+        # for i in range(len(nums)):
+        #     total_less = 0
+        #     for j in range(len(nums)):
+        #         if i != j and nums[j] < nums[i]:
+        #             total_less += 1
+        #     result[i] = total_less
+
+        # return result
+        # Time: O(nlogn)
+        # Space: O(n)
+        sorted_nums = sorted(nums)
+        mapping = {}
+        result = []
+
+        for i in range(len(sorted_nums)):
+            if sorted_nums[i] not in mapping:
+                mapping[sorted_nums[i]] = i
+
+        for i in range(len(nums)):
+            result.append(mapping[nums[i]])
+
+        return result
+
+
+# @lc code=end
