0e9581752fd12909b7c61dcf62c1e92ddda4b237
Nitish Sachar <nitish.sachar@protonmail.com>
2021-04-09 10:47:39 -0700

    852

---
+++ python3/easy/852-peak-index-in-a-mountain-array.py
@@ -0,0 +1,79 @@
+#
+# @lc app=leetcode id=852 lang=python3
+#
+# [852] Peak Index in a Mountain Array
+#
+# https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+#
+# algorithms
+# Easy (71.72%)
+# Likes:    1031
+# Dislikes: 1361
+# Total Accepted:    219.8K
+# Total Submissions: 306.3K
+# Testcase Example:  '[0,1,0]'
+#
+# Let's call an array arr a mountain if the following properties hold:
+#
+#
+# arr.length >= 3
+# There exists some i with 0 < i < arr.length - 1 such that:
+#
+# arr[0] < arr[1] < ... arr[i-1] < arr[i]
+# arr[i] > arr[i+1] > ... > arr[arr.length - 1]
+#
+#
+#
+#
+# Given an integer array arr that is guaranteed to be a mountain, return any i
+# such that arr[0] < arr[1] < ... arr[i - 1] < arr[i] > arr[i + 1] > ... >
+# arr[arr.length - 1].
+#
+#
+# Example 1:
+# Input: arr = [0,1,0]
+# Output: 1
+# Example 2:
+# Input: arr = [0,2,1,0]
+# Output: 1
+# Example 3:
+# Input: arr = [0,10,5,2]
+# Output: 1
+# Example 4:
+# Input: arr = [3,4,5,1]
+# Output: 2
+# Example 5:
+# Input: arr = [24,69,100,99,79,78,67,36,26,19]
+# Output: 2
+#
+#
+# Constraints:
+#
+#
+# 3 <= arr.length <= 10^4
+# 0 <= arr[i] <= 10^6
+# arr is guaranteed to be a mountain array.
+#
+#
+#
+# Follow up: Finding the O(n) is straightforward, could you find an O(log(n))
+# solution?
+#
+
+# @lc code=start
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        i, j = 0, len(arr) - 1
+        while i <= j:
+            mid = (i + j) >> 1
+            if arr[mid] > arr[mid - 1] and arr[mid] > arr[mid + 1]:
+                return mid
+            elif arr[mid - 1] > arr[mid]:
+                j = mid - 1
+            else:
+                i = mid + 1
+
+        return -1
+
+
+# @lc code=end
