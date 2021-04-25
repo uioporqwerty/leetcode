70197b9b9a36fc1a8ec449861431b679fd5272b0
Nitish Sachar <nitish.sachar@protonmail.com>
2021-04-25 00:48:07 -0700

    1704

---
+++ python3/easy/1704-determine-if-string-halves-are-alike.py
@@ -0,0 +1,87 @@
+#
+# @lc app=leetcode id=1704 lang=python3
+#
+# [1704] Determine if String Halves Are Alike
+#
+# https://leetcode.com/problems/determine-if-string-halves-are-alike/description/
+#
+# algorithms
+# Easy (77.41%)
+# Likes:    161
+# Dislikes: 13
+# Total Accepted:    25K
+# Total Submissions: 32.4K
+# Testcase Example:  '"book"'
+#
+# You are given a string s of even length. Split this string into two halves of
+# equal lengths, and let a be the first half and b be the second half.
+#
+# Two strings are alike if they have the same number of vowels ('a', 'e', 'i',
+# 'o', 'u', 'A', 'E', 'I', 'O', 'U'). Notice that s contains uppercase and
+# lowercase letters.
+#
+# Return true if a and b are alike. Otherwise, return false.
+#
+#
+# Example 1:
+#
+#
+# Input: s = "book"
+# Output: true
+# Explanation: a = "bo" and b = "ok". a has 1 vowel and b has 1 vowel.
+# Therefore, they are alike.
+#
+#
+# Example 2:
+#
+#
+# Input: s = "textbook"
+# Output: false
+# Explanation: a = "text" and b = "book". a has 1 vowel whereas b has 2.
+# Therefore, they are not alike.
+# Notice that the vowel o is counted twice.
+#
+#
+# Example 3:
+#
+#
+# Input: s = "MerryChristmas"
+# Output: false
+#
+#
+# Example 4:
+#
+#
+# Input: s = "AbCdEfGh"
+# Output: true
+#
+#
+#
+# Constraints:
+#
+#
+# 2 <= s.length <= 1000
+# s.length is even.
+# s consists of uppercase and lowercase letters.
+#
+#
+#
+
+# @lc code=start
+class Solution:
+    def halvesAreAlike(self, s: str) -> bool:
+        vowels = set(["a", "e", "i", "o", "u"])
+        left_count = 0
+        right_count = 0
+        i, j = 0, len(s) - 1
+        while i <= j:
+            if s[i].lower() in vowels:
+                left_count += 1
+            if s[j].lower() in vowels:
+                right_count += 1
+            i += 1
+            j -= 1
+        return left_count == right_count
+
+
+# @lc code=end
