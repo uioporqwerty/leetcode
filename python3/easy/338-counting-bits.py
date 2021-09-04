06bd0d3e56cd0399188e33c28edd97d0cc7032d8
uioporqwerty <nitish.sachar@protonmail.com>
2021-09-03 23:26:16 -0700

    338

---
+++ python3/easy/338-counting-bits.py
@@ -0,0 +1,79 @@
+#
+# @lc app=leetcode id=338 lang=python3
+#
+# [338] Counting Bits
+#
+# https://leetcode.com/problems/counting-bits/description/
+#
+# algorithms
+# Easy (70.94%)
+# Likes:    4635
+# Dislikes: 232
+# Total Accepted:    402.2K
+# Total Submissions: 562.6K
+# Testcase Example:  '2'
+#
+# Given an integer n, return an array ans of length n + 1 such that for each i
+# (0 <= i <= n), ans[i] is the number of 1's in the binary representation of
+# i.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: n = 2
+# Output: [0,1,1]
+# Explanation:
+# 0 --> 0
+# 1 --> 1
+# 2 --> 10
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: n = 5
+# Output: [0,1,1,2,1,2]
+# Explanation:
+# 0 --> 0
+# 1 --> 1
+# 2 --> 10
+# 3 --> 11
+# 4 --> 100
+# 5 --> 101
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 0 <= n <= 10^5
+# 
+# 
+# 
+# Follow up:
+# 
+# 
+# It is very easy to come up with a solution with a runtime of O(n log n). Can
+# you do it in linear time O(n) and possibly in a single pass?
+# Can you do it without using any built-in function (i.e., like
+# __builtin_popcount in C++)?
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def numSetBits(self, n: int) -> int:
+        count = 0
+        while n:
+            count += 1
+            n &= (n - 1)
+        return count
+
+    def countBits(self, n: int) -> List[int]:
+        result = []
+        for i in range(n + 1):
+            result.append(self.numSetBits(i))
+        return result
+# @lc code=end
