778e8f1d94c8cf59130f9ac8bacdfd4aa55b1c2a
uioporqwerty <nitish.sachar@protonmail.com>
2022-01-12 18:16:56 -0800

    1137

---
+++ python3/easy/1137-n-th-tribonacci-number.py
@@ -0,0 +1,58 @@
+#
+# @lc app=leetcode id=1137 lang=python3
+#
+# [1137] N-th Tribonacci Number
+#
+# https://leetcode.com/problems/n-th-tribonacci-number/description/
+#
+# algorithms
+# Easy (61.86%)
+# Likes:    1384
+# Dislikes: 88
+# Total Accepted:    198.8K
+# Total Submissions: 320.9K
+# Testcase Example:  '4'
+#
+# The Tribonacci sequence Tn is defined as follows: 
+# 
+# T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+# 
+# Given n, return the value of Tn.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: n = 4
+# Output: 4
+# Explanation:
+# T_3 = 0 + 1 + 1 = 2
+# T_4 = 1 + 1 + 2 = 4
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: n = 25
+# Output: 1389537
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 0 <= n <= 37
+# The answer is guaranteed to fit within a 32-bit integer, ie. answer <= 2^31 -
+# 1.
+# 
+#
+
+# @lc code=start
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        dp = [0, 1, 1]
+        for i in range(3, n + 1):
+            dp.append(dp[i - 1] + dp[i - 2] + dp[i - 3])
+        
+        return dp[n] 
+# @lc code=end
