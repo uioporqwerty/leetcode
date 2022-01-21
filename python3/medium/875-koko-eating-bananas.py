efc6c89e86325dfbc9fb1039d0bdb6fa7a928041
uioporqwerty <nitish.sachar@protonmail.com>
2022-01-20 17:59:14 -0800

    875

---
+++ python3/medium/875-koko-eating-bananas.py
@@ -0,0 +1,90 @@
+#
+# @lc app=leetcode id=875 lang=python3
+#
+# [875] Koko Eating Bananas
+#
+# https://leetcode.com/problems/koko-eating-bananas/description/
+#
+# algorithms
+# Medium (54.32%)
+# Likes:    2619
+# Dislikes: 138
+# Total Accepted:    117.7K
+# Total Submissions: 216.1K
+# Testcase Example:  '[3,6,7,11]\n8'
+#
+# Koko loves to eat bananas. There are n piles of bananas, the i^th pile has
+# piles[i] bananas. The guards have gone and will come back in h hours.
+# 
+# Koko can decide her bananas-per-hour eating speed of k. Each hour, she
+# chooses some pile of bananas and eats k bananas from that pile. If the pile
+# has less than k bananas, she eats all of them instead and will not eat any
+# more bananas during this hour.
+# 
+# Koko likes to eat slowly but still wants to finish eating all the bananas
+# before the guards return.
+# 
+# Return the minimum integer k such that she can eat all the bananas within h
+# hours.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: piles = [3,6,7,11], h = 8
+# Output: 4
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: piles = [30,11,23,4,20], h = 5
+# Output: 30
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: piles = [30,11,23,4,20], h = 6
+# Output: 23
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= piles.length <= 10^4
+# piles.length <= h <= 10^9
+# 1 <= piles[i] <= 10^9
+# 
+# 
+#
+
+# @lc code=start
+from math import ceil
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        max_num = max(piles)
+        i, j = 1, max_num
+        min_rate = float("inf")
+
+        while i <= j:
+            mid = (i + j) // 2
+            hours_remaining = h
+
+            for k in range(len(piles)):
+                pile = piles[k]
+                hours_remaining -= ceil(pile / mid)
+                if hours_remaining < 0:
+                    break
+
+            if hours_remaining >= 0:
+                j = mid - 1
+                min_rate = mid
+            else:
+                i = mid + 1
+        
+        return min_rate 
+
+# @lc code=end
