c2e8873c1d41b1f5000c911743d7ab3f30f949b4
uioporqwerty <nitish.sachar@protonmail.com>
2022-01-29 18:25:15 -0800

    4

--- python3/hard/4-median-of-two-sorted-arrays.py
+++ python3/hard/4-median-of-two-sorted-arrays.py
@@ -52,30 +52,62 @@
 # @lc code=start
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        merged = []
+        #O(min(A, B)) time O(1) space
+        A, B = nums1, nums2
+
+        if len(nums2) < len(nums1):
+            A, B = nums2, nums1
         
-        i, j = 0, 0
-        while i < len(nums1) and j < len(nums2):
-            if nums1[i] <= nums2[j]:
-                merged.append(nums1[i])
-                i += 1
+        total = len(A) + len(B)
+        half = total // 2
+        l, r = 0, len(A) - 1
+
+        while True:
+            i = (l + r) // 2
+            j = half - i - 2
+
+            A_left = A[i] if i >= 0 else float("-inf")
+            A_right = A[i + 1] if i + 1 < len(A) else float("inf")
+            B_left = B[j] if j >= 0 else float("-inf")
+            B_right = B[j + 1] if j + 1 < len(B)  else float("inf")
+
+            if A_left <= B_right and B_left <= A_right:
+                if total % 2:
+                    return min(A_right, B_right) 
+                else:
+                    return (max(A_left, B_left) + min(A_right, B_right)) / 2
+            elif A_left > B_right:
+                r = i - 1
             else:
-                merged.append(nums2[j])
-                j += 1
+                l = i + 1
+    
+
+
+        # O(m + n) time O(m + n) space
+        # merged = []
         
-        while i < len(nums1):
-            merged.append(nums1[i])
-            i += 1
+        # i, j = 0, 0
+        # while i < len(nums1) and j < len(nums2):
+        #     if nums1[i] <= nums2[j]:
+        #         merged.append(nums1[i])
+        #         i += 1
+        #     else:
+        #         merged.append(nums2[j])
+        #         j += 1
         
-        while j < len(nums2):
-            merged.append(nums2[j])
-            j += 1
+        # while i < len(nums1):
+        #     merged.append(nums1[i])
+        #     i += 1
         
-        if not merged:
-            return 0
+        # while j < len(nums2):
+        #     merged.append(nums2[j])
+        #     j += 1
         
-        if len(merged) % 2 == 0:
-            return (merged[len(merged) // 2] + merged[(len(merged) // 2) - 1]) / 2.0
+        # if not merged:
+        #     return 0
         
-        return merged[len(merged) // 2]
+        # if len(merged) % 2 == 0:
+        #     return (merged[len(merged) // 2] + merged[(len(merged) // 2) - 1]) / 2.0
+        
+        # return merged[len(merged) // 2]
 # @lc code=end
