ad221e975bd5d9b7860526c6637cb7e481cf4a07
uioporqwerty <nitish.sachar@protonmail.com>
2022-01-29 17:36:12 -0800

    4

---
+++ python3/hard/4-median-of-two-sorted-arrays.py
@@ -0,0 +1,81 @@
+#
+# @lc app=leetcode id=4 lang=python3
+#
+# [4] Median of Two Sorted Arrays
+#
+# https://leetcode.com/problems/median-of-two-sorted-arrays/description/
+#
+# algorithms
+# Hard (33.37%)
+# Likes:    14214
+# Dislikes: 1808
+# Total Accepted:    1.2M
+# Total Submissions: 3.6M
+# Testcase Example:  '[1,3]\n[2]'
+#
+# Given two sorted arrays nums1 and nums2 of size m and n respectively, return
+# the median of the two sorted arrays.
+# 
+# The overall run time complexity should be O(log (m+n)).
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: nums1 = [1,3], nums2 = [2]
+# Output: 2.00000
+# Explanation: merged array = [1,2,3] and median is 2.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: nums1 = [1,2], nums2 = [3,4]
+# Output: 2.50000
+# Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# nums1.length == m
+# nums2.length == n
+# 0 <= m <= 1000
+# 0 <= n <= 1000
+# 1 <= m + n <= 2000
+# -10^6 <= nums1[i], nums2[i] <= 10^6
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        merged = []
+        
+        i, j = 0, 0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] <= nums2[j]:
+                merged.append(nums1[i])
+                i += 1
+            else:
+                merged.append(nums2[j])
+                j += 1
+        
+        while i < len(nums1):
+            merged.append(nums1[i])
+            i += 1
+        
+        while j < len(nums2):
+            merged.append(nums2[j])
+            j += 1
+        
+        if not merged:
+            return 0
+        
+        if len(merged) % 2 == 0:
+            return (merged[len(merged) // 2] + merged[(len(merged) // 2) - 1]) / 2.0
+        
+        return merged[len(merged) // 2]
+# @lc code=end
