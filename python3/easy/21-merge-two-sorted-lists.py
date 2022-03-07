bd25ad8282dead43701b249a023958df309c45c0
uioporqwerty <nitish.sachar@protonmail.com>
2022-03-07 11:56:30 -0800

    21

---
+++ python3/easy/21-merge-two-sorted-lists.py
@@ -0,0 +1,89 @@
+#
+# @lc app=leetcode id=21 lang=python3
+#
+# [21] Merge Two Sorted Lists
+#
+# https://leetcode.com/problems/merge-two-sorted-lists/description/
+#
+# algorithms
+# Easy (58.89%)
+# Likes:    9874
+# Dislikes: 954
+# Total Accepted:    1.9M
+# Total Submissions: 3.1M
+# Testcase Example:  '[1,2,4]\n[1,3,4]'
+#
+# You are given the heads of two sorted linked lists list1 and list2.
+# 
+# Merge the two lists in a one sorted list. The list should be made by splicing
+# together the nodes of the first two lists.
+# 
+# Return the head of the merged linked list.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: list1 = [1,2,4], list2 = [1,3,4]
+# Output: [1,1,2,3,4,4]
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: list1 = [], list2 = []
+# Output: []
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: list1 = [], list2 = [0]
+# Output: [0]
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# The number of nodes in both lists is in the range [0, 50].
+# -100 <= Node.val <= 100
+# Both list1 and list2 are sorted in non-decreasing order.
+# 
+# 
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+
+        start = list1 if list1.val <= list2.val else list2
+
+        while list1 and list2:
+            prev = None
+            while list1 and list2 and list1.val <= list2.val:
+                prev = list1
+                list1 = list1.next
+            
+            if prev:
+                prev.next = list2
+            else:
+                while list1 and list2 and list2.val <= list1.val:
+                    prev = list2
+                    list2 = list2.next
+                if prev:
+                    prev.next = list1
+        
+        return start
+# [5]\n[1,2,4]
+
+# @lc code=end
