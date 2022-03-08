f7cb324eec9645f8e69594a41eef87bf3386f24f
uioporqwerty <nitish.sachar@protonmail.com>
2022-03-07 20:04:33 -0800

    141

--- python3/easy/141-linked-list-cycle.py
+++ python3/easy/141-linked-list-cycle.py
@@ -14,16 +14,14 @@
 
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
-        slow = head
-        fast = head
+        slow, fast = head, head
 
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-
             if slow == fast:
                 return True
-
+        
         return False
 
 
